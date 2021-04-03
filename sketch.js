dc8e241feb29dde5a314fe53f4491efed9007c9d
Chirag Khatri <74537319+Chirag963@users.noreply.github.com>
2021-04-03 13:41:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+var monkey , monkey_running; 
+var banana ,bananaImage, obstacle, obstacleImage;
+var FoodGroup, obstacleGroup;
+var score;
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png"); 
+ 
+}
+
+
+
+function setup() {
+  
+  
+  monkey = createSprite(80,315,20,20);
+  monkey.addAnimation("moving", monkey_running);
+  monkey.scale=0.1;
+  
+  banana = createSprite(200,130,10,10);
+  banana.addImage(bananaImage,"banana.png");
+  banana.velocityX = -3;
+  banana.scale = 0.1;
+  banana.lifetime = 400;
+  
+  ground = createSprite(400,350,900,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  console.log(ground.x);
+  
+}
+
+
+function draw() {
+  background("lightgrey")
+  
+ if(ground.x<0) {
+   ground.x=ground.width/2;
+ }
+  if(keyDown("space")) {
+    monkey.velocityY=-12;
+  }
+  monkey.velocityY=monkey.velocityY + 0.8;
+  
+  monkey.collide(ground);
+  
+  
+  
+ drawSprites();
+}
+
+
+
+
+
+
